ce96c2be7bf15a984c0accf2c1ffab9a0cdb90db
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-12-15 11:29:23 -0500

    Add error handling example

--- src/app/join-page/signup/signup.component.ts
+++ src/app/join-page/signup/signup.component.ts
@@ -12,6 +12,8 @@ export class SignupComponent implements OnInit {
 
   theUser = new User();
 
+  errorMessage: string;
+
   constructor(
     private userThang: UserApiService,
     private routerThang: Router
@@ -26,7 +28,14 @@ export class SignupComponent implements OnInit {
             this.routerThang.navigate(['/phones']);
         })
         .catch((err) => {
-            alert('Sorry! Something went wrong.');
+            // alert('Sorry! Something went wrong.');
+            const apiResult = err.error;
+
+            this.errorMessage = apiResult.error;
+
+            if (!this.errorMessage) {
+              this.errorMessage = "Sorry, y'all. Something went wrong.";
+            }
             console.log('Sign up error');
             console.log(err);
         });
